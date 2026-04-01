fed3ecb9cac2dd6d5c8a6b9f952ac96cdd53de42
t2025-m0135 <junwon2631@gmail.com>
2026-04-01 15:42:21 +0900

    SagaEvent 역질렬화 용도 생성자 추가

--- common/src/main/java/com/shipflow/common/messaging/event/SagaEvent.java
+++ common/src/main/java/com/shipflow/common/messaging/event/SagaEvent.java
@@ -8,13 +8,17 @@ import java.util.UUID;
 @Getter
 public abstract class SagaEvent {
 
-    private final String eventId;        // 멱등성 처리용
-    private final String eventType;      // EventType 상수값
-    private final LocalDateTime occurredAt;
+    private String eventId;        // 멱등성 처리용
+    private String eventType;      // EventType 상수값
+    private LocalDateTime occurredAt;
 
     protected SagaEvent(String eventType) {
         this.eventId = UUID.randomUUID().toString();
         this.eventType = eventType;
         this.occurredAt = LocalDateTime.now();
+    }
+
+    protected SagaEvent(){
+        // JSON 역직렬화 용도 -> 기본 생성자 필요
     }
 }
