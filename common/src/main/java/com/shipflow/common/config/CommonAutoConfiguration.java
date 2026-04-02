a13fcbdf387a8701632e2209e6632b76d680621c
t2025-m0135 <junwon2631@gmail.com>
2026-04-02 17:33:07 +0900

    Rabbit Config 조건부반영으로 수정

--- common/src/main/java/com/shipflow/common/config/CommonAutoConfiguration.java
+++ common/src/main/java/com/shipflow/common/config/CommonAutoConfiguration.java
@@ -1,9 +1,7 @@
 package com.shipflow.common.config;
 
 import com.shipflow.common.exception.ApiControllerAdvice;
-import com.shipflow.common.messaging.publisher.RabbitEventPublisher;
 import com.shipflow.config.message.JacksonConfig;
-import com.shipflow.config.message.RabbitMqConfig;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Import;
 @Import({
         ApiControllerAdvice.class,
         JacksonConfig.class,
-        RabbitMqConfig.class,
-        RabbitEventPublisher.class
 })
 public class CommonAutoConfiguration {
 }
