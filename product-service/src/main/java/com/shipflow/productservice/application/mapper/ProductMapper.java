7c81caf97ce67e105bbe4e8994c677303afdd306
soo <dlgustn96@gmail.com>
2026-04-04 08:39:10 +0900

    chore(product): ProductMapper, ProductRepositoryImpl 백틱 제거

--- product-service/src/main/java/com/shipflow/productservice/application/mapper/ProductMapper.java
+++ product-service/src/main/java/com/shipflow/productservice/application/mapper/ProductMapper.java
@@ -9,15 +9,12 @@ import com.shipflow.productservice.presentation.dto.response.ProductInfoResponse
 import com.shipflow.productservice.presentation.dto.response.ProductListResponse;
 import com.shipflow.productservice.presentation.dto.response.ProductUpdateResponse;
 
-`@Mapper`(componentModel ="spring")
-
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
 	//Entity->DTO
 	ProductCreateResponse toCreateResponse(Product product);
 
-	`
-	@Mapping`(target ="updateAt",source ="updatedAt")
-
+	@Mapping(target = "updateAt", source = "updatedAt")
 	ProductUpdateResponse toUpdateResponse(Product product);
 
 	ProductInfoResponse toProductInfoResponse(Product product);
