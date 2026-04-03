a6ac3892aefce238117a8195250ecf39450af73d
250 <ghgh2505380@gmail.com>
2026-04-03 14:22:16 +0900

    refactor(slack): 검증 추가(#4)

--- notification-service/src/main/java/com/shipflow/notificationservice/infrastructure/client/slack/config/SlackProperties.java
+++ notification-service/src/main/java/com/shipflow/notificationservice/infrastructure/client/slack/config/SlackProperties.java
@@ -1,10 +1,15 @@
 package com.shipflow.notificationservice.infrastructure.client.slack.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "slack")
+@Validated
 public class SlackProperties {
 
+	@NotBlank
 	private String botToken;
 
 	public String getBotToken() {
