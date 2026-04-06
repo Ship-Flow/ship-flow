1d7235a093b4ffe084dd36a17e977e82ce29ba9a
250 <ghgh2505380@gmail.com>
2026-04-06 23:32:40 +0900

    feat(notification): 유레카 서버 포트 설정 추가 (#30)

--- notification-service/src/main/java/com/shipflow/notificationservice/NotificationserviceApplication.java
+++ notification-service/src/main/java/com/shipflow/notificationservice/NotificationserviceApplication.java
@@ -3,9 +3,11 @@ package com.shipflow.notificationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableFeignClients
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
